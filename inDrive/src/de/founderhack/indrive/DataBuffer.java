ee3623503735d3d83019a615cc8929c240bf245e
Lars Tholen <Lars.Tholen@rwth-aachen.de>
2013-11-18 23:31:46 +0100

    Dummy Daten

--- inDrive/src/de/founderhack/indrive/DataBuffer.java
+++ inDrive/src/de/founderhack/indrive/DataBuffer.java
@@ -1,7 +1,9 @@
 package de.founderhack.indrive;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import android.content.Context;
 import de.founderhack.indrive.dsa.DiagnosticValue;
@@ -18,14 +20,33 @@ public class DataBuffer {
 	public List<DiagnosticValue> speed = new ArrayList<DiagnosticValue>();
 	public List<DiagnosticValue> accelerationPedal = new ArrayList<DiagnosticValue>();
 	public List<DiagnosticValue> range = new ArrayList<DiagnosticValue>();
+	Random generator = new Random();
+	
+	private List<DiagnosticValue> getDummyList() {		
+		List<DiagnosticValue> dummyList =  new ArrayList<DiagnosticValue>();
+		for (int i=1;i<10;i++) {
+			dummyList.add(new DiagnosticValue("dummy", generator.nextInt(), "dummy"));
+		}
+		return dummyList;
+	}
 	
 	private DataBuffer(Context ctx) {
 		context = ctx;
+		fuelReserve.addAll(getDummyList());
+		temp.addAll(getDummyList());
+		oilTemp.addAll(getDummyList());
+		brightness.addAll(getDummyList());
+		coolantTemp.addAll(getDummyList());
+		rpm.addAll(getDummyList());
+		speed.addAll(getDummyList());
+		accelerationPedal.addAll(getDummyList());
+		range.addAll(getDummyList());
 	}
 	
 	 public static DataBuffer getInstance(Context context) {
 	        if (instance == null) {
-	            instance = new DataBuffer(context.getApplicationContext());           
+	            instance = new DataBuffer(context.getApplicationContext()); 
+	            
 	        }
 	        return instance;
 	 }
