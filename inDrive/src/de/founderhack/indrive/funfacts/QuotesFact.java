0840edef1fddaab956a445512d84bd5e391a26cb
Lars Tholen <Lars.Tholen@rwth-aachen.de>
2013-11-19 03:41:50 +0100

    KitKats!

--- inDrive/src/de/founderhack/indrive/funfacts/QuotesFact.java
+++ inDrive/src/de/founderhack/indrive/funfacts/QuotesFact.java
@@ -9,7 +9,6 @@ public class QuotesFact implements Fact {
 	private static Random mRnd = new Random();
 	@SuppressWarnings("serial")
 	private static ArrayList<String> mQuotes = new ArrayList<String>(){{
-		add("Für Deinen zurückglegten Weg könntest Du 1394 Kit Kats a 45 Gramm essen.");
 		add("Würdest Du Schafswolle tanken, müssten wegen Dir schon 305 Schafe geschoren worden sein.");
 		add("So fährt man Auto!\nDu könntest Dir von Deinem gesparten Geld ein neues Paar Turnschuhe bestellen ;-)");
 	}};
